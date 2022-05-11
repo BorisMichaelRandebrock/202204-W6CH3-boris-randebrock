c21279d04136e43c6aaa2f5c2447fcb0a857193d
Boris <boris@randebrock.com>
2022-05-11 20:21:07 +0200

    initial phoneSlice added

---
+++ src/redux/feature/phoneSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const phoneSlice = createSlice({
+  name: "phone",
+  initialState: {
+    phoneNumber: [],
+    calling: false,
+  },
+  reducers: {
+    addDigit: (phone, action) => ({
+      ...phone,
+      phoneNumber: [...phone, action.payload],
+    }),
+  },
+});
+
+export const { addDigit: addDigitActionCreator } = phoneSlice.actions;
+
+export default phoneSlice;
